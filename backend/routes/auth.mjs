527babbe3d550dff6a0bc42b6e91d587e0c00c0e
Kiran Khayamali <kirankhayamali@gmail.com>
2025-07-27 15:03:13 +0545

    Implementing user authentication using facebook

--- backend/routes/auth.mjs
+++ backend/routes/auth.mjs
@@ -11,5 +11,13 @@ router.get("/auth/google/callback",
     }
 );
 
+router.get("/auth/facebook", passport.authenticate("facebook", {scope: ["email", "public_profile"]}));
+router.get("/auth/facebook/callback",
+    passport.authenticate("facebook", {failureRedirect: "/"}),
+    (req, res) => {
+        res.redirect("/profile"); //this send the user status
+    }
+);
+
 export default router;
 
