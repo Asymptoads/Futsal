f50131a97e245d1713a337edf4500dbb3ff0c16c
nimeshakya <nimeshakyabtw@gmail.com>
2025-07-29 22:25:17 +0545

    Created Sys adming login page.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 
 import Home from './Pages/Home';
 
+// Sys adming pages
+import Login from './Pages/SysAdmin/Login';
+
 const App = () => {
     return (
         <Routes>
             <Route path='/' element={<Home />} />
+
+            {/* System Admin Routes */}
+            <Route path='/sysadmin/login' element={<Login/>} />
         </Routes>
     );
 };
