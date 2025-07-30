57a129adbf37cd24a2d078980ae19810cb4eb83b
nimeshakya <nimeshakyabtw@gmail.com>
2025-07-30 18:35:18 +0545

    Added chart labels.

--- frontend/src/Pages/SysAdmin/SysAdminHome.tsx
+++ frontend/src/Pages/SysAdmin/SysAdminHome.tsx
@@ -1,4 +1,4 @@
-import { HStack } from '@chakra-ui/react';
+import { HStack, VStack, Text } from '@chakra-ui/react';
 import SysAdminPageContainer from '../../components/SysAdminPageContainer/SysAdminPageContainer';
 import { Chart, useChart } from '@chakra-ui/charts';
 import {
@@ -27,91 +27,97 @@ const SysAdminHome = () => {
             <HStack
                 width={'100%'}
                 height={'100%'}
-                gap={'40px'}
+                gap={'100px'}
                 padding={'20px'}
             >
-                <Chart.Root maxH='md' chart={chart}>
-                    <LineChart data={chart.data}>
-                        <CartesianGrid
-                            stroke={chart.color('border')}
-                            vertical={false}
-                        />
-                        <XAxis
-                            axisLine={false}
-                            dataKey={chart.key('month')}
-                            tickFormatter={(value) => value.slice(0, 3)}
-                            stroke={chart.color('border')}
-                            label={{ value: 'Month', position: 'bottom' }}
-                        />
-                        <YAxis
-                            axisLine={false}
-                            tickLine={false}
-                            tickMargin={10}
-                            stroke={chart.color('border')}
-                            label={{
-                                value: 'Customers',
-                                position: 'left',
-                                angle: -90,
-                            }}
-                        />
-                        <Tooltip
-                            animationDuration={100}
-                            cursor={false}
-                            content={<Chart.Tooltip />}
-                        />
-                        {chart.series.map((item) => (
-                            <Line
-                                key={item.name}
-                                isAnimationActive={false}
-                                dataKey={chart.key(item.name)}
-                                stroke={chart.color(item.color)}
-                                strokeWidth={2}
-                                dot={false}
+                <VStack flex={1} gap={'40px'}>
+                    <Chart.Root maxH='md' chart={chart}>
+                        <LineChart data={chart.data}>
+                            <CartesianGrid
+                                stroke={chart.color('border')}
+                                vertical={false}
                             />
-                        ))}
-                    </LineChart>
-                </Chart.Root>
-                <Chart.Root maxH='md' chart={chart}>
-                    <LineChart data={chart.data}>
-                        <CartesianGrid
-                            stroke={chart.color('border')}
-                            vertical={false}
-                        />
-                        <XAxis
-                            axisLine={false}
-                            dataKey={chart.key('month')}
-                            tickFormatter={(value) => value.slice(0, 3)}
-                            stroke={chart.color('border')}
-                            label={{ value: 'Month', position: 'bottom' }}
-                        />
-                        <YAxis
-                            axisLine={false}
-                            tickLine={false}
-                            tickMargin={10}
-                            stroke={chart.color('border')}
-                            label={{
-                                value: 'Customers',
-                                position: 'left',
-                                angle: -90,
-                            }}
-                        />
-                        <Tooltip
-                            animationDuration={100}
-                            cursor={false}
-                            content={<Chart.Tooltip />}
-                        />
-                        {chart.series.map((item) => (
-                            <Line
-                                key={item.name}
-                                isAnimationActive={false}
-                                dataKey={chart.key(item.name)}
-                                stroke={chart.color(item.color)}
-                                strokeWidth={2}
-                                dot={false}
+                            <XAxis
+                                axisLine={false}
+                                dataKey={chart.key('month')}
+                                tickFormatter={(value) => value.slice(0, 3)}
+                                stroke={chart.color('border')}
+                                label={{ value: 'Month', position: 'bottom' }}
                             />
-                        ))}
-                    </LineChart>
-                </Chart.Root>
+                            <YAxis
+                                axisLine={false}
+                                tickLine={false}
+                                tickMargin={10}
+                                stroke={chart.color('border')}
+                                label={{
+                                    value: 'Customers',
+                                    position: 'left',
+                                    angle: -90,
+                                }}
+                            />
+                            <Tooltip
+                                animationDuration={100}
+                                cursor={false}
+                                content={<Chart.Tooltip />}
+                            />
+                            {chart.series.map((item) => (
+                                <Line
+                                    key={item.name}
+                                    isAnimationActive={false}
+                                    dataKey={chart.key(item.name)}
+                                    stroke={chart.color(item.color)}
+                                    strokeWidth={2}
+                                    dot={false}
+                                />
+                            ))}
+                        </LineChart>
+                    </Chart.Root>
+                    <Text fontSize={'24px'}>User Growth</Text>
+                </VStack>
+                <VStack flex={1} gap={'40px'}>
+                    <Chart.Root maxH='md' chart={chart}>
+                        <LineChart data={chart.data}>
+                            <CartesianGrid
+                                stroke={chart.color('border')}
+                                vertical={false}
+                            />
+                            <XAxis
+                                axisLine={false}
+                                dataKey={chart.key('month')}
+                                tickFormatter={(value) => value.slice(0, 3)}
+                                stroke={chart.color('border')}
+                                label={{ value: 'Month', position: 'bottom' }}
+                            />
+                            <YAxis
+                                axisLine={false}
+                                tickLine={false}
+                                tickMargin={10}
+                                stroke={chart.color('border')}
+                                label={{
+                                    value: 'Customers',
+                                    position: 'left',
+                                    angle: -90,
+                                }}
+                            />
+                            <Tooltip
+                                animationDuration={100}
+                                cursor={false}
+                                content={<Chart.Tooltip />}
+                            />
+                            {chart.series.map((item) => (
+                                <Line
+                                    key={item.name}
+                                    isAnimationActive={false}
+                                    dataKey={chart.key(item.name)}
+                                    stroke={chart.color(item.color)}
+                                    strokeWidth={2}
+                                    dot={false}
+                                />
+                            ))}
+                        </LineChart>
+                    </Chart.Root>
+                    <Text fontSize={'24px'}>Futsal Growth</Text>
+                </VStack>
             </HStack>
         </SysAdminPageContainer>
     );
